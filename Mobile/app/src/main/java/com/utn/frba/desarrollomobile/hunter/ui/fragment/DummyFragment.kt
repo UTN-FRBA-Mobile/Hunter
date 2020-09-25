1c8c10d77e2317aeb1393a7e74dccc5d28942be5
diegotabares <diego.e.tabares@gmail.com>
2020-09-25 16:33:39 -0300

    adding fragment navigation extensions and animation transitions

---
+++ Mobile/app/src/main/java/com/utn/frba/desarrollomobile/hunter/ui/fragment/DummyFragment.kt
@@ -0,0 +1,7 @@
+package com.utn.frba.desarrollomobile.hunter.ui.fragment
+
+import androidx.fragment.app.Fragment
+import com.utn.frba.desarrollomobile.hunter.R
+
+class DummyFragment : Fragment(R.layout.fragment_dummy) {
+}
