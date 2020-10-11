880f4d3a9e2e6d838076388d9e5c0ea5130bb8be
tmanoja <tmanoja@gmail.com>
2020-10-11 20:17:27 -0300

    -use database env

--- Backend/Hunter/Program.cs
+++ Backend/Hunter/Program.cs
@@ -9,8 +9,8 @@ namespace Hunter
     {
         public static void Main(string[] args)
         {
-            //Database.postgresContext.ConnectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
-            Database.postgresContext.ConnectionString = "postgres://ouaqzwgabbxhxt:0687c2478f0521a595632a35a7638252b71b6db73f35badddf40bcc5fdc2153d@ec2-3-216-92-193.compute-1.amazonaws.com:5432/d5qpniim1huq2a";
+            Database.postgresContext.ConnectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
+            //Database.postgresContext.ConnectionString = "postgres://ouaqzwgabbxhxt:0687c2478f0521a595632a35a7638252b71b6db73f35badddf40bcc5fdc2153d@ec2-3-216-92-193.compute-1.amazonaws.com:5432/d5qpniim1huq2a";
             CreateHostBuilder(args).Build().Run();
         }
 
