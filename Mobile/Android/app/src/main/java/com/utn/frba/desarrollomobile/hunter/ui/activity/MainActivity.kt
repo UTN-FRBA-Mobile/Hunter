9c60d24ef44439df337cadc5f22c52e0e2c9183f
Leandro Angiolini <angiolini.leandro@gmail.com>
2020-10-12 18:54:16 -0300

    upload image when game create. Backend not implemented

--- Mobile/Android/app/src/main/java/com/utn/frba/desarrollomobile/hunter/ui/activity/MainActivity.kt
+++ Mobile/Android/app/src/main/java/com/utn/frba/desarrollomobile/hunter/ui/activity/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.utn.frba.desarrollomobile.hunter.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.utn.frba.desarrollomobile.hunter.LoginFragment
 import com.utn.frba.desarrollomobile.hunter.R
 import com.utn.frba.desarrollomobile.hunter.extensions.showFragment
 import com.utn.frba.desarrollomobile.hunter.ui.fragment.AppFragment
+import com.utn.frba.desarrollomobile.hunter.ui.fragment.ChooseGameFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +19,14 @@ class MainActivity : AppCompatActivity() {
 
         val auth = FirebaseAuth.getInstance()
         auth.addAuthStateListener { firebaseAuth ->
+            //Todo Borrar
             if(firebaseAuth.currentUser != null) {
+                firebaseAuth.currentUser?.getIdToken(true)?.addOnCompleteListener{task ->
+                    if(task.isSuccessful){
+                        Log.d("DEBUG", task.result?.token.toString())
+                    }
+                }
+            // ----
                 goToApp()
             } else {
                 goToLogin()
@@ -34,6 +43,6 @@ class MainActivity : AppCompatActivity() {
     private fun goToApp() {
         supportActionBar?.show()
         actionBar?.show()
-        showFragment(AppFragment(), false)
+        showFragment(ChooseGameFragment(), false)
     }
 }
