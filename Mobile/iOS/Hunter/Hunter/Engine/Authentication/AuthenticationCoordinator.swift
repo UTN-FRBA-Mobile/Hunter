8e2a5331010b0966de2017a4c0a4c1cbdfbdd843
Franco Leto <letoefranco@gmail.com>
2020-10-26 17:59:34 -0300

    Add AuthenticationCoordinator

--- Mobile/iOS/Hunter/Hunter/Engine/Authentication/AuthenticationCoordinator.swift
+++ Mobile/iOS/Hunter/Hunter/Engine/Authentication/AuthenticationCoordinator.swift
@@ -3,6 +3,7 @@
 import Foundation
 
 protocol AuthenticationFlow {
+
 }
 
 protocol AuthenticationCaseUse {
@@ -20,5 +21,33 @@ class AuthenticationCoordinator<CaseUse: AuthenticationCaseUse, Flow: Authentica
     
     func start() {
         
+    }
+}
+
+import UIKit
+class AuthenticationRouter<Nav: UINavigationController,
+                           Factory: AuthenticationFactory>: AuthenticationFlow, Router {
+    let navigation: Nav
+    let factory: Factory
+    
+    init(navigation: Nav, factory: Factory) {
+        self.navigation = navigation
+        self.factory = factory
+    }
+    
+    func showAuthentication() {
+        show(factory.authenticationScreen())
+    }
+    
+}
+
+import UIKit
+protocol AuthenticationFactory {
+    func authenticationScreen() -> UIViewController
+}
+
+class AuthenticationViewResolver: AuthenticationFactory {
+    func authenticationScreen() -> UIViewController {
+        UIViewController()
     }
 }
