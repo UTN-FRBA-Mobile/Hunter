f028765f1df80916a138d93614443b4803d31466
tmanoja <tmanoja@gmail.com>
2020-11-01 01:44:15 -0300

    -heroku url

--- Mobile/Android/app/src/main/java/com/utn/frba/desarrollomobile/hunter/service/APIAdapter.kt
+++ Mobile/Android/app/src/main/java/com/utn/frba/desarrollomobile/hunter/service/APIAdapter.kt
@@ -5,8 +5,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object APIAdapter {
-    //val ROOT_URL = "https://utn-mobile-hunter.herokuapp.com"
-    val ROOT_URL = "http://192.168.0.153:5000"
+    val ROOT_URL = "https://utn-mobile-hunter.herokuapp.com"
+    //val ROOT_URL = "http://192.168.0.153:5000"
     var Token = ""
 
     fun createConection(): APIService? {
