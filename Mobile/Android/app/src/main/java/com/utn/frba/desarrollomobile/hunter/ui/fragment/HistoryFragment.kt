787c11e27a4dc0d9d67a9c815e879d8576446501
diegotabares <diego.e.tabares@gmail.com>
2020-11-29 21:45:17 -0300

    refactor game history item

--- Mobile/Android/app/src/main/java/com/utn/frba/desarrollomobile/hunter/ui/fragment/HistoryFragment.kt
+++ Mobile/Android/app/src/main/java/com/utn/frba/desarrollomobile/hunter/ui/fragment/HistoryFragment.kt
@@ -4,9 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
@@ -15,14 +14,13 @@ import com.utn.frba.desarrollomobile.hunter.extensions.setToolbarTitle
 import com.utn.frba.desarrollomobile.hunter.service.APIAdapter
 import com.utn.frba.desarrollomobile.hunter.service.models.Game
 import com.utn.frba.desarrollomobile.hunter.service.models.User
+import com.utn.frba.desarrollomobile.hunter.ui.activity.MainActivity
 import com.utn.frba.desarrollomobile.hunter.utils.LoginHandler
 import kotlinx.android.synthetic.main.fragment_history.*
-import kotlinx.android.synthetic.main.fragment_register.*
-import kotlinx.android.synthetic.main.view_listitem_lost_game.view.*
+import kotlinx.android.synthetic.main.view_listitem_win_game.view.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.lang.Exception
 
 class HistoryFragment : Fragment(R.layout.fragment_history) {
 
@@ -31,14 +29,15 @@ class HistoryFragment : Fragment(R.layout.fragment_history) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        showLoading()
+
         if (LoginHandler.USERID == 0) {
-            var callGetUserResponse =
+            val callGetUserResponse =
                 APIAdapter.getAPI().getUser()
 
             callGetUserResponse.enqueue(object : Callback<User> {
                 override fun onFailure(call: Call<User>, t: Throwable) {
                     print("throw Message" + t.message)
-                    register_password_confirmation.error = "Error reading JSON"
                 }
 
                 override fun onResponse(
@@ -58,13 +57,13 @@ class HistoryFragment : Fragment(R.layout.fragment_history) {
         val viewManager = LinearLayoutManager(this.context)
         var viewAdapter = GamesAdapter(myDataset)
 
-        var callGameHistoryResponse =
+        val callGameHistoryResponse =
             APIAdapter.getAPI().getMyHistoryGames()
 
         callGameHistoryResponse.enqueue(object : Callback<ArrayList<Game>> {
             override fun onFailure(call: Call<ArrayList<Game>>, t: Throwable) {
                 print("throw Message" + t.message)
-                register_password_confirmation.error = "Error reading JSON"
+                hideLoading()
             }
 
             override fun onResponse(
@@ -81,6 +80,7 @@ class HistoryFragment : Fragment(R.layout.fragment_history) {
                         layoutManager = viewManager
                         adapter = viewAdapter
                     }
+                    hideLoading()
                 }
             }
         })
@@ -88,12 +88,25 @@ class HistoryFragment : Fragment(R.layout.fragment_history) {
         recyclerView = recycler_history_games.apply {
             layoutManager = viewManager
             adapter = viewAdapter
+            val dividerItemDecoration = DividerItemDecoration(
+                this.context,
+                LinearLayoutManager.VERTICAL
+            )
+            addItemDecoration(dividerItemDecoration)
         }
     }
 
     override fun onResume() {
         super.onResume()
         setToolbarTitle(getString(R.string.historyGame))
+    }
+
+    private fun showLoading() {
+        (activity as MainActivity).showLoading("Cargando...")
+    }
+
+    private fun hideLoading() {
+        (activity as MainActivity).hideLoading()
     }
 }
 
@@ -102,25 +115,9 @@ class GamesAdapter(private val myDataset: MutableList<Game>) :
 
     override fun getItemCount() = myDataset.size
 
-    override fun getItemViewType(position: Int): Int {
-        if(myDataset[position].winId != null)
-        {
-            val winID: Int = myDataset[position].winId ?: 0
-            if (winID == LoginHandler.USERID)
-                return 1
-        }
-
-        return 0
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GamesAdapter.MyViewHolder {
-        val view : View = if(viewType == 1)
-            LayoutInflater.from(parent.context)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view: View = LayoutInflater.from(parent.context)
                 .inflate(R.layout.view_listitem_win_game, parent, false)
-        else
-            LayoutInflater.from(parent.context)
-                .inflate(R.layout.view_listitem_lost_game, parent, false)
-
         return MyViewHolder(view)
     }
 
@@ -132,6 +129,11 @@ class GamesAdapter(private val myDataset: MutableList<Game>) :
 
         fun bindGame(game: Game) {
             itemView.game_date.text = game.endDatetime?.substring(0, 10)
+            if (game.winId == LoginHandler.USERID) {
+                itemView.game_winner.text = "GANÉ!!!"
+            } else {
+                itemView.game_winner.text = "Perdí..."
+            }
 
             if (!game.photo.isNullOrEmpty()) {
                 try {
