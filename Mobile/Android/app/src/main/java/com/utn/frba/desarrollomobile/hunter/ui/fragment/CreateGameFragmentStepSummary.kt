240b4d8fe33fb592a1be38de168e33a1a4036eb1
Leandro Angiolini <angiolini.leandro@gmail.com>
2020-11-29 23:30:56 -0300

    fix bitmap

--- Mobile/Android/app/src/main/java/com/utn/frba/desarrollomobile/hunter/ui/fragment/CreateGameFragmentStepSummary.kt
+++ Mobile/Android/app/src/main/java/com/utn/frba/desarrollomobile/hunter/ui/fragment/CreateGameFragmentStepSummary.kt
@@ -9,16 +9,17 @@ import android.util.Log
 import android.os.Environment
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.zxing.BarcodeFormat
 import com.journeyapps.barcodescanner.BarcodeEncoder
-import com.google.android.gms.maps.GoogleMap
 import com.utn.frba.desarrollomobile.hunter.R
 import com.utn.frba.desarrollomobile.hunter.extensions.showFragment
 import com.utn.frba.desarrollomobile.hunter.service.models.Game
 import com.utn.frba.desarrollomobile.hunter.viewmodel.CreateGameViewModel
+import kotlinx.android.synthetic.main.fragment_create_game_step_add_image.view.*
 import kotlinx.android.synthetic.main.fragment_create_game_step_summary.*
 import java.io.File
 import java.io.FileOutputStream
@@ -29,21 +30,10 @@ class CreateGameFragmentStepSummary : Fragment(R.layout.fragment_create_game_ste
     private lateinit var gameViewModel: CreateGameViewModel
     private var game: Game? = null
 
-    private lateinit var googleMap: GoogleMap
-
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        //map.onCreate(savedInstanceState)
 
         gameViewModel = ViewModelProvider(requireActivity()).get(CreateGameViewModel::class.java)
-
-//        gameViewModel.getImage().observe(viewLifecycleOwner, Observer { image ->
-//            imagePreview.setImageBitmap(image)
-//        })
-//
-//        gameViewModel.getClue().observe(viewLifecycleOwner, Observer { text ->
-//            clue.text = text
-//        })
 
         gameViewModel.getGameCreated().observe(viewLifecycleOwner, Observer { game ->
                 this.game = game
@@ -60,6 +50,10 @@ class CreateGameFragmentStepSummary : Fragment(R.layout.fragment_create_game_ste
     private fun updateUI() {
         if(this.game != null) {
             try {
+                val barcodeEncoder = BarcodeEncoder()
+                val bitmap: Bitmap = barcodeEncoder.encodeBitmap(this.game!!.winCode, BarcodeFormat.QR_CODE, 512, 512)
+                qr.setImageBitmap(bitmap)
+
                 download_qr.setOnClickListener {
                     if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                         ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_WRITE_EXTERNAL_STORAGE)
@@ -90,15 +84,11 @@ class CreateGameFragmentStepSummary : Fragment(R.layout.fragment_create_game_ste
     }
 
     private fun downloadQR(game: Game) {
-        val barcodeEncoder = BarcodeEncoder()
-        val bitmap: Bitmap = barcodeEncoder.encodeBitmap(game.winCode, BarcodeFormat.QR_CODE, 512, 512)
-        qr.setImageBitmap(bitmap)
-
         val storageDir: File? = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
         val file = File.createTempFile("win_qr_${game.id}_", ".jpg", storageDir)
 
         val fOut = FileOutputStream(file)
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fOut)
+        qr.drawable.toBitmap().compress(Bitmap.CompressFormat.JPEG, 100, fOut)
         fOut.flush()
         fOut.close()
 
