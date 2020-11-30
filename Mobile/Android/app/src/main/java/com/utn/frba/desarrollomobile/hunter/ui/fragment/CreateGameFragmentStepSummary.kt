515c1b7a420c438eb48edff09796e03edbd3df90
Leandro Angiolini <angiolini.leandro@gmail.com>
2020-11-29 22:52:47 -0300

    Import conflict

--- Mobile/Android/app/src/main/java/com/utn/frba/desarrollomobile/hunter/ui/fragment/CreateGameFragmentStepSummary.kt
+++ Mobile/Android/app/src/main/java/com/utn/frba/desarrollomobile/hunter/ui/fragment/CreateGameFragmentStepSummary.kt
@@ -4,10 +4,7 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.os.Environment
-import android.util.Log
-import android.content.Intent
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
